2262a7115ec5f04eafa6640dde50c78eeada1798
Sarthak Joshi <sarthak@settlemint.com>
2022-09-19 19:56:06 +0530

    added t&c, privacy-policy, in react app

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,6 @@
 // @ts-nocheck
 import Header from './components/Header';
 import Footer from './components/Footer';
-import About from './components/About';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
             </>
           )}
         />
-        <Route path="/about" component={About} />
         <Footer />
       </div>
     </Router>
--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -4,8 +4,26 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy; 2021</p>
-      <Link to="/about">About</Link>
+      <p>Copyright &copy; 2022</p>
+      <p> &nbsp;</p>
+      <Link
+        to={{
+          pathname: 'https://docs.settlemint.com/docs/privacy-policy',
+        }}
+        target="_blank"
+      >
+        Privacy
+      </Link>
+      <p> &nbsp;</p>
+      <Link
+        to={{
+          pathname:
+            'https://docs.settlemint.com/docs/terms-of-service',
+        }}
+        target="_blank"
+      >
+        Terms & Conditions
+      </Link>
     </footer>
   );
 };
